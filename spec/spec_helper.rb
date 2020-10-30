64ba614edebd6dca1f0edd29e64fe1a4aad3bc3b
rafahg <r.hgar78@gmail.com>
2020-10-30 17:17:56 +0000

    Setup project completed.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
+# Set environment to "test"
+ENV['RACK_ENV'] = 'test'
+
+# Bring in the contents of the 'app.rb' file. The below is equivalent
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# Tell Capybara to talk to BookmarkManager 
+
+Capybara.app = BookmarkManager
+
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console
