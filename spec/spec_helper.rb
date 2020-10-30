5107a812d3192c57b55b7207248239e327632457
rafahg <r.hgar78@gmail.com>
2020-10-30 17:58:45 +0000

    First test, implemented and passed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,6 @@ require 'rspec'
 
 Capybara.app = BookmarkManager
 
-
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console
   # Want a nice code coverage website? Uncomment this next line!
