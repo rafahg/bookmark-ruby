30d95b9b7ce75d52469f9759993570634e80960f
rafahg <r.hgar78@gmail.com>
2020-11-01 12:24:53 +0000

    Test for adding a new bookmark passed

--- app.rb
+++ app.rb
@@ -13,5 +13,12 @@ class BookmarkManager < Sinatra::Base
   get '/bookmarks/new' do
     erb :'bookmarks/new' # view
   end
+
+  post '/bookmarks' do
+    url = params['url']
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}');")
+    redirect '/bookmarks'
+  end
   run! if app_file == $0
 end
