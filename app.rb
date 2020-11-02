f7d6101c10fbdd0a94b6da929f7179995115a6be
rafahg <r.hgar78@gmail.com>
2020-11-02 14:42:03 +0000

    Update test unit test, passed, Update refactored in app.rb

--- app.rb
+++ app.rb
@@ -31,9 +31,8 @@ class BookmarkManager < Sinatra::Base
   end
 
   patch '/bookmarks/:id' do
-
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("UPDATE bookmarks SET url = '#{params[:url]}', title = '#{params[:title]}' WHERE id = '#{params[:id]}'")
+    p params
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
     
     redirect('/bookmarks')
   end
